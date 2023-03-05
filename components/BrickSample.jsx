d01edb70dcf9ba80236a15b5b95397fd5c1b1b8e
LordPrinz <plazinski2902@gmail.com>
2023-03-05 20:14:56 +0100

    Fix extension

--- components/BrickSample.jsx
+++ components/BrickSample.jsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 const BrickSample = (props) => {
 	const { color } = props;
 
+	const extension = color.startsWith("true") ? "gif" : "png";
+
 	return (
 		<div className="flex items-center content-start gap-4 text-[#ddd] px-20 hover:bg-[#323232] py-4 cursor-pointer select-none transition hover:transition">
 			<Image
 				alt={`${color} brick`}
-				src={`/bricks/brick--${color}.png`}
+				src={`/bricks/brick--${color}.${extension}`}
 				width="80"
 				height="80"
 			/>
