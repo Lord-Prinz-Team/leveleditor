b2df97ea159e3d43be794ef6557306bc0a194fe6
LordPrinz <plazinski2902@gmail.com>
2023-03-05 19:34:59 +0100

    Add main scene

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
 import background from "../public/background.png";
+import sidebricks from "../public/left_right-border.png";
+import topbrick from "../public/border-top.png";
+
 const Screen = () => {
 	return (
-		<div className="w-[1280px] h-[720px]">
+		<div className="w-[1280px] h-[720px] relative overflow-hidden">
 			<Image alt="background" src={background} />
+			<Image alt="top bricks" className="absolute top-0 z-10" src={topbrick} />
+			<Image alt="left bricks" className="absolute top-0" src={sidebricks} />
+			<Image
+				alt="right bricks"
+				className="absolute top-0 right-0"
+				src={sidebricks}
+			/>
 		</div>
 	);
 };
