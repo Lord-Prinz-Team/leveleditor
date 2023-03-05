a08c44bcc19e7eee7384248d7b83f74d718e0064
LordPrinz <plazinski2902@gmail.com>
2023-03-05 20:42:25 +0100

    Add react drop package

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import MainCanvas from "@/components/MainCanvas";
+import Head from "next/head";
 
 export default function Home() {
 	return (
 		<>
+			<Head>
+				<title>Level Editor</title>
+			</Head>
 			<div className="bg-[#303030] h-full w-full flex items-center justify-center">
 				<MainCanvas />
 			</div>
