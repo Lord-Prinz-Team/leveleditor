b68f6d7c70345376de7f64982979601932c7f62d
LordPrinz <plazinski2902@gmail.com>
2023-03-05 19:40:00 +0100

    Add brick graphic

--- components/BlocksDrawer.jsx
+++ components/BlocksDrawer.jsx
@@ -1,5 +1,13 @@
+import BrickSample from "./BrickSample";
+
 const BlocksDrawer = () => {
-	return <div className="border">a</div>;
+	return (
+		<div className="border">
+			<div className="container">
+				<BrickSample />
+			</div>
+		</div>
+	);
 };
 
 export default BlocksDrawer;
--- components/BrickSample.jsx
+++ components/BrickSample.jsx
@@ -3,3 +3,5 @@ const BrickSample = (props) => {
 
 	return <div>{name}</div>;
 };
+
+export default BrickSample;
