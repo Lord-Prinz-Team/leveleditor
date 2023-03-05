3a4774b53a63f85b0b851154d124d332430ba654
LordPrinz <plazinski2902@gmail.com>
2023-03-05 19:44:06 +0100

    Add brick sample

--- components/BrickSample.jsx
+++ components/BrickSample.jsx
@@ -1,7 +1,18 @@
-const BrickSample = (props) => {
-	const { graphic, name } = props;
+import Image from "next/image";
 
-	return <div>{name}</div>;
+const BrickSample = (props) => {
+	const { color } = props;
+
+	return (
+		<div>
+			<Image
+				alt={`${color} brick`}
+				src={`/bricks/brick--${color}.png`}
+				width="64"
+				height="64"
+			/>
+		</div>
+	);
 };
 
 export default BrickSample;
